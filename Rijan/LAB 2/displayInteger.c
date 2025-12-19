f030b1d85b707c088a532c4d808c8e157eadfa40
Rijan Dahal <70582929+enkoki@users.noreply.github.com>
2025-12-19 16:21:27 +0545

    Lab 2 Questions

---
+++ Rijan/LAB 2/displayInteger.c
@@ -0,0 +1,9 @@
+// Write a program to input an integer from the user and display the entered value.
+#include <stdio.h>
+int main(){
+    int num;
+    printf("Enter any number: ");
+    scanf("%d", &num);
+    printf("Your number is %d.", num);
+    return 0;
+}
