bbaf4a215e389bed8d75ef435cf0030f9b6f0b33
Rijan Dahal <70582929+enkoki@users.noreply.github.com>
2025-12-29 12:18:40 +0545

    Testing

---
+++ Rijan/bit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int num;
+    printf("Enter any number: ");
+    scanf("%d", &num);
+    if(num & 1){
+        printf("It is odd");
+    }
+    else{
+        printf("It is even");
+    }
+    return 0;
+}
